6b49e2ed5d3a1019e6dd8ea17f8692c1f9857635
Oleksandr Khomenko <aleksandr.kh.galanix@gmail.com>
2022-11-23 00:55:08 +0200

    add: some validation rules

--- index.js
+++ index.js
@@ -123,18 +123,104 @@ bot.on('text', ctx => {
     const messageText = ctx.message.text.replace(/@sweet_dreams_tgbot /g, ''); ///// @sweet_dreams_tgbot - telegram bot name
     ctx.session.messageText = messageText;
 
+    // check on urls
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    if (urlRegex.test(messageText)) {
+      ctx.reply('Посилання в повідомленні заборонені\n\n Спробуй ще раз');
+      return;
+    }
+
+    // check symbol count
     if (messageText.length <= 100) {
-      ctx.replyWithHTML(
+      ctx.reply(
         'Твій сон повинен бути більше 100 символів(\n\n Спробуй ще раз',
       );
-    } else {
-      ctx.replyWithHTML(
-        'Ти хочеш анонімно поділитися наступним сном:\n\n' +
-          `<i>“${messageText}”</i>\n\n` +
-          'Чи все вірно?',
-        sendEditKeyboard(messageText),
-      );
+      return;
     }
+
+    // check bed worlds
+    const badWorlds = [
+      'хохлы',
+      'пидор',
+      'пидорас',
+      'пидрила',
+      'підор',
+      'нігер',
+      'ніга',
+      'www',
+      'com',
+    ];
+    const hasHaveBadWorld = badWorlds.find(world =>
+      messageText.includes(world),
+    );
+    if (hasHaveBadWorld) {
+      ctx.replyWithHTML(
+        `В тексті сну не повинно бути поганих слів, як <i>“${hasHaveBadWorld}”</i>\n\n Спробуй ще раз`,
+      );
+      return;
+    }
+
+    // check zalgo text
+    const hasZalgo = /%CC%/g.test(encodeURIComponent(messageText));
+    if (hasZalgo) {
+      ctx.replyWithHTML(
+        `В тексті сну не повинно бути Zalgo тексту\n\n Спробуй ще раз`,
+      );
+      return;
+    }
+
+    // check repeated worlds
+    if (checkRepeatedInMessage('world', 4, messageText)) {
+      ctx.replyWithHTML(
+        `Слова часто повторюються підряд, схоже на спам\n\n Спробуй ще раз`,
+      );
+      return;
+    }
+
+    // check repeated symbol
+    if (checkRepeatedInMessage('symbol', 8, messageText)) {
+      ctx.replyWithHTML(
+        `Букви часто повторюються підряд, схоже на спам\n\n Спробуй ще раз`,
+      );
+      return;
+    }
+
+    function checkRepeatedInMessage(type, brakeCount, message) {
+
+      let stringElement;
+      if (type === 'world') {
+        stringElement = message.split(' ');
+      } else if (type === 'symbol') {
+        stringElement = message.split('');
+      }
+
+      let counter = 1;
+      let previousWorld;
+      let currentWorld;
+
+      for (let i = 0; i < stringElement.length; i++) {
+        currentWorld = stringElement[i];
+
+        if (previousWorld === currentWorld) {
+          counter = counter + 1;
+        } else {
+          counter = 1;
+        }
+
+        if (counter === brakeCount) {
+          return true;
+        }
+        previousWorld = currentWorld;
+      }
+      return false;
+    }
+
+    ctx.replyWithHTML(
+      'Ти хочеш анонімно поділитися наступним сном:\n\n' +
+        `<i>“${messageText}”</i>\n\n` +
+        'Чи все вірно?',
+      sendEditKeyboard(messageText),
+    );
   } else {
     ctx.reply('Нажміть /start щоб почати');
   }
